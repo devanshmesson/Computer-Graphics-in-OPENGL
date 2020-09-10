48404a3d02d21d0d14d10c2bd1c1440288b2a0ef
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-09-10 10:12:02 +0530

    Create Bresenham_circle.cpp

---
+++ Bresenham_circle.cpp
@@ -0,0 +1,80 @@
+#include<GL/gl.h>
+#include<GL/glu.h>
+#include<GL/glut.h>
+#include<math.h>
+#include<stdio.h>
+
+float x,y,Radius;
+float xx=0,yy=Radius;
+void make(int xx,int yy,int x,int y)
+{
+
+      glVertex2f(xx+x,yy+y);
+      glVertex2f(xx-x,yy+y);
+      glVertex2f(xx+x,yy-y);
+      glVertex2f(xx-x,yy-y);
+      glVertex2f(xx+y,yy+x);
+      glVertex2f(xx-y,yy+x);
+      glVertex2f(xx+y,yy-x);
+      glVertex2f(xx-y,yy-x);
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    gluOrtho2D(-100,100,-100,100);
+    glColor3d(0,1,0);
+    glBegin(GL_POLYGON);
+
+    int distance=3-2*Radius;
+
+      glVertex2f(xx+x,yy+y);
+      glVertex2f(xx-x,yy+y);
+      glVertex2f(xx+x,yy-y);
+      glVertex2f(xx-x,yy-y);
+      glVertex2f(xx+y,yy+x);
+      glVertex2f(xx-y,yy+x);
+      glVertex2f(xx+y,yy-x);
+      glVertex2f(xx-y,yy-x);
+
+    while(y>=x)
+    {
+        x++;
+        if(distance>0)
+        {
+            y--;
+            distance+=4*(x-y)+10;
+        }
+        else distance+=4*x+6;
+
+      glVertex2f(xx+x,yy+y);
+      glVertex2f(xx-x,yy+y);
+      glVertex2f(xx+x,yy-y);
+      glVertex2f(xx-x,yy-y);
+      glVertex2f(xx+y,yy+x);
+      glVertex2f(xx-y,yy+x);
+      glVertex2f(xx+y,yy-x);
+      glVertex2f(xx-y,yy-x);
+    }
+    glEnd();
+    glutSwapBuffers();
+
+}
+
+int main(int argc,char **argv)
+{
+   printf("Enter Radius of the Circle\n");
+   scanf("%f",&Radius);
+   printf("Enter Coordinates of the Circle\n");
+   scanf("%f",&x);
+   scanf("%f",&y);
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+  glutInitWindowPosition(100,200);
+  glutInitWindowSize(500,500);
+  glClearColor(1,1,1,1);
+  glutCreateWindow("Circle using Polar Coordinaates");
+  glutDisplayFunc(display);
+  glutMainLoop();
+}
