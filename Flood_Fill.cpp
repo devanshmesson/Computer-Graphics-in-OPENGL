828ce9e4ddccb94f8b4d4227a71ce639b5b56979
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-09-24 10:38:07 +0530

    Update Flood_Fill.cpp

--- Flood_Fill.cpp
+++ Flood_Fill.cpp
@@ -1,97 +1,121 @@
+#include <bits/stdc++.h>
 #include <math.h>
-#include <gl/gl.h>
-#include <gl/glu.h>
-#include <gl/glut.h>
+#include <time.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+using namespace std;
 
-struct Point {
-	GLint x;
-	GLint y;
-};
+int n,a,b,choice=1,maxy=INT_MIN,maxx=INT_MIN,minx=INT_MAX,miny=INT_MAX;
+float oldColor[3]={1,1,1};
+float newColor[3]={1,0,0};
+vector<pair<int,int>>v;
 
-struct Color {
-	GLfloat r;
-	GLfloat g;
-	GLfloat b;
-};
 
-void init() {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
-	glColor3f(0.0, 0.0, 0.0);
-	glPointSize(1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0, 640, 0, 480);
-}
 
-Color getPixelColor(GLint x, GLint y) {
-	Color color;
-	glReadPixels(x, y, 1, 1, GL_RGB, GL_FLOAT, &color);
-	return color;
-}
-
-void setPixelColor(GLint x, GLint y, Color color) {
-	glColor3f(color.r, color.g, color.b);
-	glBegin(GL_POINTS);
-	glVertex2i(x, y);
-	glEnd();
-	glFlush();
-}
-
-void floodFill(GLint x, GLint y, Color oldColor, Color newColor) {
-	Color color;
-	color = getPixelColor(x, y);
-
-	if(color.r == oldColor.r && color.g == oldColor.g && color.b == oldColor.b)
-	{
-		setPixelColor(x, y, newColor);
-		floodFill(x+1, y, oldColor, newColor);
-		floodFill(x, y+1, oldColor, newColor);
-		floodFill(x-1, y, oldColor, newColor);
-		floodFill(x, y-1, oldColor, newColor);
-	}
-	return;
-}
-
-void onMouseClick(int button, int state, int x, int y)
+void init()
 {
-	Color newColor = {1.0f, 0.0f, 0.0f};
-	Color oldColor = {1.0f, 1.0f, 1.0f};
-
-	floodFill(320, 240, oldColor, newColor);
+    glClearColor(1.0,1.0,1.0,1.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0,640,0,480);
 }
 
-void draw_circle(Point pC, GLfloat radius) {
-	GLfloat step = 1/radius;
-	GLfloat x, y;
+void floodfill(int x, int y, float oldColor[], float newColor[]) //Flood Fill Algorithm
+{
+    if(y>=minx && y<=maxy && x>=minx && x<=maxx)
+    {
 
-	for(GLfloat theta = 0; theta <= 360; theta += step) {
-		x = pC.x + (radius * cos(theta));
-		y = pC.y + (radius * sin(theta));
-		glVertex2i(x, y);
-	}
+    float color[3];
+    glReadPixels(x,y,1.0,1.0,GL_RGB,GL_FLOAT,color); //Checking the colour of the present pixel
+
+    int flag=1;
+    for(int i=0;i<3;i++)
+    {
+        if(color[i]!=oldColor[i]){flag=0;break;} //if(present pixel colour == oldcolour)then colour present pixel with newcolor
+    }
+        if(flag==1)
+        {
+        glColor3f(newColor[0],newColor[1],newColor[2]);//colouring the pixel
+        glBegin(GL_POINTS);
+        glVertex2i(x,y); //plotting that coloured pixel
+        glEnd();
+        glFlush();
+        if(choice==0)
+        {
+         floodfill(x+1,y,oldColor,newColor);
+         floodfill(x-1,y,oldColor,newColor);
+         floodfill(x,y+1,oldColor,newColor);
+         floodfill(x,y-1,oldColor,newColor);
+        }
+        else if(choice==1)
+        {
+         floodfill(x+1,y,oldColor,newColor);
+         floodfill(x-1,y,oldColor,newColor);
+         floodfill(x,y+1,oldColor,newColor);
+         floodfill(x,y-1,oldColor,newColor);
+
+         floodfill(x+1,y+1,oldColor,newColor);
+         floodfill(x-1,y+1,oldColor,newColor);
+         floodfill(x-1,y-1,oldColor,newColor);
+         floodfill(x+1,y-1,oldColor,newColor);
+        }
+
+        }
+    }
+    }
+
+
+void mouse(int btn, int state, int x, int y) //starting the algorithm from the point
+                                             //on which the mouse is clicked
+{
+    y = 480-y;
+    if(btn==GLUT_LEFT_BUTTON)
+    {
+        if(state==GLUT_DOWN)
+        {
+            floodfill(x,y,oldColor,newColor);
+        }
+    }
 }
 
-void display(void) {
-	Point pt = {320, 240};
-	GLfloat radius = 50;
-
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POINTS);
-		draw_circle(pt, radius);
-	glEnd();
-	glFlush();
+void display()  //Drawing the Polygon
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0,0,1);
+    glBegin(GL_LINE_LOOP);
+    printf("Enter the Coordinates:");
+    for(int i=0;i<n;i++)
+    {
+        glVertex2i(v[i].first,v[i].second);
+    }
+    glEnd();
+    glFlush();
 }
 
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(200, 200);
-	glutCreateWindow("Open GL");
-	init();
-	glutDisplayFunc(display);
-	glutMouseFunc(onMouseClick);
-	glutMainLoop();
-	return 0;
+    printf("Enter Number of Coordinates for the Polygon:");
+    scanf("%d",&n);
+    printf("Enter the Coordinates:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d%d",&a,&b);
+        v.push_back({a,b});
+        if(maxy<b)maxy=b;
+        if(maxx<a)maxx=a;
+        if(miny>b)miny=b;
+        if(minx>a)minx=a;
+    }
+    printf("Press 0 for 4-Connected\n Press 1 for 8-Connected");
+    scanf("%d",&choice);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(200,200);
+    glutCreateWindow("Flood Fill Algorithm");
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    init();
+    glutMainLoop();
+    return 0;
 }
